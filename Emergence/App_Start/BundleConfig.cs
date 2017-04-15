dec12a413d6ea221adb60d3d22f897f3f85cdc17
Ben Sheron <benmosheron@gmail.com>
2017-04-15 19:14:22 +0100

    Remove MVC boilerplate.
    Also rejigged structure and started the index.

--- Emergence/App_Start/BundleConfig.cs
+++ Emergence/App_Start/BundleConfig.cs
@@ -8,6 +8,9 @@ namespace Emergence
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            // Bundle of all common js files
+            bundles.Add(new ScriptBundle("~/bundles/common").IncludeDirectory("~/Scripts/Common", "*js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
---
+++ Emergence/Scripts/Emergence/Index.js
@@ -0,0 +1,3 @@
+﻿window.onload = function () {
+    let ctx = $("#canvasIndex0")[0].getContext('2d');
+}
