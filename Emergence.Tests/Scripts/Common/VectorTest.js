d93953090f1fbb36ac2bc41051714b5e2c80653d
Ben Sheron <benmosheron@gmail.com>
2017-05-06 16:19:07 +0100

    more Vector.js unit tests.

--- Emergence.Tests/Scripts/Common/VectorTest.js
+++ Emergence.Tests/Scripts/Common/VectorTest.js
@@ -1,19 +1,20 @@
 ﻿// Unit tests for Vector.js
 // Run with:
-// > mocha --recursive .
-//c:\emergence\emergence\scripts\common\vector.js
+// > mocha --recursive .\Emergence.Tests
+
 var assert = require('assert');
 var vector = require('../../../Emergence/Scripts/Common/Vector.js');
 
 let Vector = vector.Vector();
 
 describe("Vector", function () {
-    let vZero = Vector.create([0, 0, 0]);
+    
     // arbitrary x,y,z values
     let x = 1.01;
     let y = -39.01;
     let z = 1093.1239;
     describe("#create()", function () {
+
         it("should throw if the argument is not an array", function () {
             assert.throws(function () { Vector.create(); });
             assert.throws(function () { Vector.create(null); });
@@ -57,6 +58,7 @@ describe("Vector", function () {
             for (var i = 0; i < N; i++) { assert.strictEqual(v.array[i], a[i]); }
         });
     });
+
     describe("#create2()", function () {
         it("should throw if no argument is provided", function () {
             assert.throws(Vector.create2);
@@ -76,6 +78,7 @@ describe("Vector", function () {
             assert.strictEqual(v.y, y);
         });
     });
+
     describe("#createRandom()", function () {
         it("should create an N-dimensional random vector with elements between min and max", function () {
             let N = 1000;
@@ -90,6 +93,7 @@ describe("Vector", function () {
             }
         })
     });
+
     describe("v.map()", function () {
         it("should provide an analogue to Array.map()", function () {
             let v = Vector.create([x, y, z]);
@@ -101,6 +105,7 @@ describe("Vector", function () {
             assert.strictEqual(vDoubled.z, z * 2);
         });
     });
+
     describe("v.reduce()", function () {
         it("should provide an analogue to Array.reduce()", function () {
             let v = Vector.create([x, y, z]);
@@ -108,69 +113,158 @@ describe("Vector", function () {
             assert.strictEqual(r, x + y + z);
         });
     });
+
     describe("#magnitude()", function () {
-        let v = Vector.create([x, y, z]);
-        function assertMagnitude(m) {
-            assert.strictEqual(m, Math.sqrt((x * x) + (y * y) + (z * z)));
-        }
-        describe("static", function () {
-            it("should calculate the magnitude of the vector argument", function () {
-                let m = Vector.magnitude(v);
-                assertMagnitude(m);
-            });
-            it("should return zero if the vector argument has all elements === 0", function () {
-                let m = Vector.magnitude(vZero);
-                assert.strictEqual(m, 0);
-            });
+        it("should calculate the magnitude of a 2-vector", function () {
+            let v = Vector.create([3, 4]); // 5
+            let m = doBoth(
+                () => Vector.magnitude(v),
+                () => v.magnitude());
+            assert.strictEqual(m, 5);
         });
-        describe("member", function () {
-            it("should calculate the magnitude of itself", function () {
-                let m = v.magnitude();
-                assertMagnitude(m);
-            });
-            it("should return zero if all its elements === 0", function () {
-                let m = vZero.magnitude();
-                assert.strictEqual(m, 0);
-            });
+        it("should calculate the magnitude of a 3-vector", function () {
+            let v = Vector.create([1, -2, 2]); // 3
+            let m = doBoth(
+                () => Vector.magnitude(v),
+                () => v.magnitude());
+            assert.strictEqual(m, 3);
+        });
+        it("should calculate the magnitude of a longer vector", function () {
+            let v = Vector.create([4, -2, 1, 1, 1, 1, 1]) // 5
+            let m = doBoth(
+                () => Vector.magnitude(v),
+                () => v.magnitude());
+            assert.strictEqual(m, 5);
+        });
+        it("should return zero if the vector has all elements === 0", function () {
+            let v = Vector.create([0, 0, 0]);
+            let m = doBoth(
+                () => Vector.magnitude(v),
+                () => v.magnitude());
+            assert.strictEqual(m, 0);
         });
     });
+
     describe("#normalise()", function () {
-        let v = Vector.create([x, y, z]);
-        function assertNormalised(vNorm) {
-            isAVector(vNorm);
-            assert(basicallyEqual(vNorm.magnitude(), 1));
-            assert(basicallyEqual(vNorm.x, x / Math.sqrt((x * x) + (y * y) + (z * z))));
-            assert(basicallyEqual(vNorm.y, y / Math.sqrt((x * x) + (y * y) + (z * z))));
-            assert(basicallyEqual(vNorm.z, z / Math.sqrt((x * x) + (y * y) + (z * z))));
+        function doNormaliseAssertions(normalised, original, expected) {
+            isAVector(normalised);
+            assert.strictEqual(normalised.length, original.length, "normalised vector should have the same length as the unnormalised.");
+            if (!normalised.array.every(e => e === 0))
+                assert.strictEqual(normalised.magnitude(), 1, "normalised vector should have magnitude 1.");
+            else
+                assert.strictEqual(normalised.magnitude(), 0, "normalised vector of a zero vectorshould have magnitude 0.");
+            assertVectorsBasicallyEqual(normalised, expected);
         }
-        function assertZeroVector(zeroVector, expectedLength) {
-            isAVector(zeroVector);
-            assert.strictEqual(zeroVector.length, expectedLength);
-            for (var i = 0; i < zeroVector.length; i++) {
-                assert.strictEqual(zeroVector.array[i], 0);
-            }
-        }
-        describe("static", function () {
-            it("should calculate the normalised vector of the vector argument", function () {
-                let vNorm = Vector.normalise(v);
-                assertNormalised(vNorm);
-            });
-            it("should return a zero vector for a zero vector argument", function () {
-                let vNorm = Vector.normalise(vZero);
-                assertZeroVector(vNorm, vZero.length);
-            });
+        it("should normalise a 2-vector", function () {
+            let v = Vector.create([3, 4]); //5
+            let n = doBoth(
+                () => Vector.normalise(v),
+                () => v.normalise(),
+                assertVectorsBasicallyEqual);
+            doNormaliseAssertions(n, v, Vector.create([3 / 5, 4 / 5]));
         });
-        describe("member", function () {
-            it("should calculate the normalised vector of itself", function () {
-                let vNorm = v.normalise();
-                assertNormalised(vNorm);
-            });
-            it("should return a zero vector if all elements === 0", function () {
-                let vNorm = vZero.normalise();
-                assertZeroVector(vNorm, vZero.length);
-            });
+        it("should normalise a 3-vector", function () {
+            let v = Vector.create([1, -2, 2]); //3
+            let n = doBoth(
+                () => Vector.normalise(v),
+                () => v.normalise(),
+                assertVectorsBasicallyEqual);
+            doNormaliseAssertions(n, v, Vector.create([1 / 3, -2 / 3, 2 / 3]));
+        });
+        it("should normalise a longer vector", function () {
+            let v = Vector.create([4, -2, 1, 1, 1, 1, 1]); //5
+            let n = doBoth(
+                () => Vector.normalise(v),
+                () => v.normalise(),
+                assertVectorsBasicallyEqual);
+            doNormaliseAssertions(n, v, Vector.create([4 / 5, -2 / 5, 1 / 5, 1 / 5, 1 / 5, 1 / 5, 1 / 5]));
+        });
+        it("should normalise a vector of zeros to a vector of zeros", function () {
+            let v = Vector.create([0, 0, 0]);
+            let n = doBoth(
+                () => Vector.normalise(v),
+                () => v.normalise(),
+                assertVectorsBasicallyEqual);
+            doNormaliseAssertions(n, v, Vector.create([0, 0, 0]));
         });
     });
+
+    describe("#add()", function () {
+        it("should add two vectors", function () {
+            let v1 = Vector.create([1, -2, 3]);
+            let v2 = Vector.create([2, 2, 2]);
+            let r = doBoth(
+                () => Vector.add(v1, v2),
+                () => v1.add(v2),
+                assertVectorsBasicallyEqual);
+            assertVectorsBasicallyEqual(r, Vector.create([3, 0, 5]));
+        });
+        it("should throw if vectors have different lengths", function () {
+            let v1 = Vector.create([1, -2]);
+            let v2 = Vector.create([2, 2, 3]);
+            let r = doBoth(
+                () => Vector.add(v1, v2),
+                () => v1.add(v2));
+            assert(r.errors.both, "Both static and member functions should have thrown.");
+        });
+    });
+
+    describe("#sub()", function () {
+        it("should subtract one vector from another", function () {
+            let v1 = Vector.create([1, -2, 3]);
+            let v2 = Vector.create([2, 2, 2]);
+            let r = doBoth(
+                () => Vector.sub(v1, v2),
+                () => v1.sub(v2),
+                assertVectorsBasicallyEqual);
+            assertVectorsBasicallyEqual(r, Vector.create([-1, -4, 1]));
+        });
+        it("should throw if vectors have different lengths", function () {
+            let v1 = Vector.create([1, -2]);
+            let v2 = Vector.create([2, 2, 3]);
+            let r = doBoth(
+                () => Vector.sub(v1, v2),
+                () => v1.sub(v2));
+            assert(r.errors.both, "Both static and member functions should have thrown.");
+        });
+    });
+
+    describe("#multiplyScalar()", function () {
+        it("should multiply a vector and a scalar", function () {
+            let v = Vector.create([1, -2, 3]);
+            let s = -10;
+            let r = doBoth(
+                () => Vector.multiplyScalar(v, s),
+                () => v1.multiplyScalar(s),
+                assertVectorsBasicallyEqual);
+            assertVectorsBasicallyEqual(r, Vector.create([-10, 20, -30]));
+        });
+    });
+
+    describe("#divideScalar()", function () {
+        it("should divide a vector by a scalar", function () {
+            let v = Vector.create([1, -2, 3]);
+            let s = -10;
+            let r = doBoth(
+                () => Vector.divideScalar(v, s),
+                () => v1.divideScalar(s),
+                assertVectorsBasicallyEqual);
+            assertVectorsBasicallyEqual(r, Vector.create([-0.1, 0.2, -0.3]));
+        });
+    });
+
+    describe("#floor()", function () {
+        it("should produce the element-wise floor function", function () {
+            let v = Vector.create([1.02312, -23.1239, 159.3213]);
+            let s = -10;
+            let r = doBoth(
+                () => Vector.floor(v),
+                () => v1.floor(),
+                assertVectorsExactlyEqual);
+            assertVectorsExactlyEqual(r, Vector.create([1, -24, 159]));
+        });
+    });
+
 });
 
 function isAVector(v) {
@@ -179,24 +273,71 @@ function isAVector(v) {
     assert.strictEqual("number", typeof v.length);
 }
 
+function doBoth(staticFunction, memberFunction, assertion) {
+    let errors = {any: false};
+    let s;
+    let m;
+
+    try{
+        s = staticFunction();
+    }
+    catch (ex) {
+        errors.any = true;
+        errors.static = true;
+    }
+
+    try {
+        m = staticFunction();
+    }
+    catch (ex) {
+        errors.any = true;
+        errors.member = true;
+    }
+    if (typeof assertion === "undefined") {
+        assert.deepStrictEqual(s, m, `Static and member functions produced different results. Static [${s}]. Member [${m}].`);
+    }
+    else {
+        assertion(s, m);
+    }
+
+    if (errors.any) {
+        errors.both = errors.static && errors.member;
+        return {errors: errors};
+    }
+
+    return m;
+}
+
+function assertVectorsBasicallyEqual(v1, v2) {
+    //console.log(`v1: [${v1.x}, ${v1.y}, ${v1.z}]`);
+    //console.log(`v2: [${v2.x}, ${v2.y}, ${v2.z}]`);
+    isAVector(v1);
+    isAVector(v2);
+    assert.strictEqual(v1.length, v2.length, "Vectors have different lengths.")
+    for (var i = 0; i < v1.length; i++) {
+        assert(
+            basicallyEqual(v1.array[i], v2.array[i]),
+            `Vector arrays differ at element [${i}]. v1: [${v1.array[i]}]. v2: [${v2.array[i]}].`);
+    }
+}
+
+function assertVectorsExactlyEqual(v1, v2) {
+    //console.log(`v1: [${v1.x}, ${v1.y}, ${v1.z}]`);
+    //console.log(`v2: [${v2.x}, ${v2.y}, ${v2.z}]`);
+    isAVector(v1);
+    isAVector(v2);
+    assert.strictEqual(v1.length, v2.length, "Vectors have different lengths.")
+    for (var i = 0; i < v1.length; i++) {
+        assert(
+            v1.array[i] === v2.array[i],
+            `Vector arrays differ at element [${i}]. v1: [${v1.array[i]}]. v2: [${v2.array[i]}].`);
+    }
+}
+
 function basicallyEqual(n1, n2) {
-    let precision = 19;
+    let absoluteTol = 0.000000001;
     if ((typeof n1 !== "number") || (typeof n2 !== "number")) throw "Only use basicallyEqual for numbers.";
     if (n1 === n2) return true;
-    // E.g. 0.0009233692949769974 ==  0.0009233692949769973
-    // or  -0.0009233692949769974 == -0.0009233692949769973
-    let n1s = n1.toString();
-    let n2s = n2.toString();
-
-    // Check we are only comparing high precision floating points
-    if (n1s.indexOf(".") !== n2s.indexOf(".")) return false;
-    // The . should either be position 1 or 2 (for negative numbers)
-    if ((n1s.indexOf(".") !== 1) && (n1s.indexOf(".") !== 2)) return false;
-
-    // truncate to precision
-    if (n1s.length > precision) n1s = n1s.substr(0, precision);
-    if (n2s.length > precision) n2s = n2s.substr(0, precision);
-
-    if (n1s === n2s) return true;
-    return false;
+    let d = Math.abs(n1 - n2);
+    return d < absoluteTol;
 }
